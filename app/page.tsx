0d6e39ea3f2f2d3a929e4c35439a4d967b849393
sammykirigha <dkirigha18@gmail.com>
2024-07-07 23:01:43 +0300

    modified the carousel

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <main className="">
-      <div className="bg-[url('../public/twenty-one.jpg')] opacity-90 relative  bg-cover bg-no-repeat h-screen">
+      <div className="bg-[url('../public/beach-two.jpg')] opacity-90 relative  bg-cover bg-no-repeat h-screen">
         <Navbar />
         <div className=" w-fit flex mx-10 items-start justify-start h-auto absolute top-[20%]">
           <h1
@@ -20,9 +20,9 @@ export default function Home() {
           <p className=" text-white text-[18px] leading-[20px] font-[400]">
             Unlock the wonders of the world: Your dream trip.
             Our tours offers a diverse range of destination that cater for a wide variety of interests </p>
-          <button className=" px-10 py-4 font-bold text-[#faf038] text-[20px] leading-4 outline outline-2 outline-[#faf038] mt-10 mb-2 w-fit rounded-lg">Book a Tour</button>
+          <button className=" px-10 py-4 font-bold bg-[#ff5722] text-white text-[20px] leading-4 outline outline-2 outline-[#ff5722] mt-10 mb-2 w-fit rounded-lg">Book a Tour</button>
         </div>
-        <div className=" absolute top-[40%] right-0 w-auto">
+        <div className=" absolute top-[50%] right-0 w-[700px]">
           <div className=" mx-2">
             <HomeCarousel />
           </div>
--- components/Carousel/HomeCarousel.tsx
+++ components/Carousel/HomeCarousel.tsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import { FaStar } from 'react-icons/fa'
+"use client"
 
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { FaArrowLeft, FaArrowRight, FaStar } from 'react-icons/fa'
+import image_one from "../../public/beach-ten.jpg"
+import image_two from "../../public/beach-seven.jpg"
+import image_three from "../../public/beach-sixteen.jpg"
 
 export default function HomeCarousel() {
+    const [disableNextButton, setDisableNextButton] = useState(false)
+    //track the image
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [transitionDirection, setTransitionDirection] = useState("next");
+
+
     return (
-        <div>
-            <div className="bg-[url('../public/beach-four.jpg')] opacity-90 rounded-lg min-w-[400px] min-h-[230px] bg-cover bg-no-repeats max-w-auto flex flex-col ">
-                <p className='absolute right-5 m-1 flex items-center px-2 justify-center gap-2 rounded-full bg-gray-800'>
+        <div className='carousel-container flex gap-10 flex-1 items-center justify-between content-between '>
+            <div className="relative content-container bg-[url('../public/beach-four.jpg')] opacity-90 rounded-lg min-w-[400px] min-h-[230px] max-h-[230px] bg-cover bg-no-repeats flex-col ">
+                <div className='absolute right-1 mt-2 flex items-center px-2 justify-center gap-2 rounded-full bg-gray-800'>
                     <span className=' text-white text-[14px]'>7.6</span>
                     <FaStar className='text-[#faf038]' />
-                </p>
-                <div className=' absolute bottom-0 flex flex-col gap-[1px] px-3 py-2 bg-transparent/20'>
+                </div>
+                <div className=' absolute bottom-0 flex flex-col gap-[1px] px-3 py-2 '>
                     <p className=' text-white text-[10px] '>USA</p>
                     <p className=' text-white text-[14px] '>Hawaii</p>
                     <p className=' text-white text-[18px] '>Hawaii Beach</p>
                 </div>
             </div>
+            <div className='image-container right-5 flex flex-row-reverse relative items-center justify-center gap-2 h-auto w-auto'>
+                <div className=' firstContainer h-auto z-20 absolute right-5 w-[200px]'>
+                    <Image className=' first-image rounded-md border-[2px] border-[#ff5722]' src={image_one} alt='' />
+                </div>
+                <div className=' firstContainer h-auto z-10 absolute right-5 w-[200px]'>
+                    <Image className=' second-image rounded-md border-[2px] border-[#ff5722]' src={image_two} alt='' />
+                </div>
+                <div className=' firstContainer h-auto z-0 absolute right-5 w-[200px]'>
+                    <Image className=' third-image rounded-md border-[2px] border-[#ff5722]' src={image_three} alt='' />
+                </div>
+
+                <div className='controls flex mt-14 gap-5 absolute right-32 z-50 translate-y-20 '>
+                    <button className=' rounded-full p-3 border-[3px] border-[#ff5722]'>
+                        <FaArrowLeft className=' text-white' />
+                    </button>
+                    <button disabled={disableNextButton} className={`${disableNextButton ? 'cursor-not-allowed' : ' '} rounded-full p-3 border-[3px] border-[#ff5722]`}>
+                        <FaArrowRight className={`${disableNextButton ? 'text-gray-200 cursor-not-allowed' : 'text-white '}`} />
+                    </button>
+                </div>
+            </div>
+
 
         </div>
     )
