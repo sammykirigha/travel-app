05cacb38ddb8d2c920102c174b7cbe8636a2197f
sammykirigha <dkirigha18@gmail.com>
2024-07-17 11:17:25 +0300

    implemented login

--- app/api/auth/confirm/route.ts
+++ app/api/auth/confirm/route.ts
@@ -1,7 +1,6 @@
+import { createServerClient, serializeCookieHeader } from '@supabase/ssr'
 import { type EmailOtpType } from '@supabase/supabase-js'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-import {supabaseServerClient} from '@/@/lib/supabaseServer'
 
 function stringOrFirstString(item: string | string[] | undefined) {
   return Array.isArray(item) ? item[0] : item
@@ -20,7 +19,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   let next = '/error'
 
   if (token_hash && type) {
-    const supabase = supabaseServerClient()
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+          cookies: {
+              getAll() {
+                return Object.keys(req.cookies).map((name) => ({ name, value: req.cookies[name] || '' }))
+              },
+              setAll(cookiesToSet) {
+                res.setHeader(
+                  'Set-Cookie',
+                  cookiesToSet.map(({ name, value, options }) => serializeCookieHeader(name, value, options))
+                )
+              },
+            },
+      }
+    )
     const { error } = await supabase.auth.verifyOtp({
       type: type as EmailOtpType,
       token_hash,
